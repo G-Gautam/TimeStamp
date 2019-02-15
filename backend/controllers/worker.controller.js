791fe699306044fd7556b046b3dfc3a5d083152b
G-Gautam <35315739+ggupta24@users.noreply.github.com>
2019-02-14 20:37:42 -0500

    backend 1.0

--- backend/controllers/worker.controller.js
+++ backend/controllers/worker.controller.js
@@ -1,5 +1,6 @@
 const Worker = require('../models/worker.model');
 var date = new Date();
+var shifts = []
 //Simple version, without validation or sanitation
 exports.shift_create = function (req, res) {
     let shift = new Worker(
@@ -19,3 +20,42 @@ exports.shift_create = function (req, res) {
         res.send('Shift Created successfully')
     })
 };
+function calHours(time){
+    startTime = time;
+    endTime = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+    var intSTime = startTime.split(":");
+    var intETime = endTime.split(":");
+
+    var hours;
+
+    if(parseInt(intETime[1]) >= parseInt(intSTime[1])){
+        //get the hours and then add the minutes - simple calculation
+        hours = (parseInt(intETime[0]) - parseInt(intSTime[0])) + ((parseInt(intETime[1]) - parseInt(intSTime[1]))/60)
+    }
+
+    else if(parseInt(intETime[1]) < parseInt(intSTime[1])){
+        //get the minutes to complete start time minutes to the end time minutes
+        var min = parseInt(intETime[1]) + (60 - parseInt(intSTime[1]))
+        var hours = parseInt(intETime[0]) - parseInt(intSTime[0]) - 1 + min/60
+    }
+    return hours;
+}
+exports.shift_update = function (req,res){
+    var date = new Date
+    Worker.findOneAndUpdate({"employerId": req.params.empId}, {$set: 
+        {"endTime": date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds(),
+         "hours": calHours(req.params.startTime) }}, function(err){
+        if (err) return next(err);
+        res.send('Shift udpated.');
+    })
+};
+
+exports.shift_between = function(req, res){
+    Worker.find({ date: { $gt: new Date(req.params.sDate)},
+    date: {$lte: new Date(req.params.eDate)},
+    employerId: {$eq: req.params.empId}}, function(err, result){
+        if(err) return next(err);
+        res.send(result)
+    })
+ }
--- backend/routes/worker.route.js
+++ backend/routes/worker.route.js
@@ -5,4 +5,8 @@ const worker_controller = require("../controllers/worker.controller");
 
 router.post('/create', worker_controller.shift_create);
 
+router.put('/:empId/:startTime/update', worker_controller.shift_update);
+
+router.get('/:empId/:sDate/:eDate/allshifts', worker_controller.shift_between);
+
 module.exports = router;
